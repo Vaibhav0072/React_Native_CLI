cdf8be4f7e72ff37f2ede03fb308886e510c1e1e
vaibhav-rao <66874462+vaibhav-rao@users.noreply.github.com>
2022-10-06 16:41:48 +0530

    Axios changes

--- src/ScreenB.js
+++ src/ScreenB.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   StyleSheet,
   View,
   Text,
   Button
 } from 'react-native';
+import axios from 'axios';
 
 
 export default function ScreenB ({navigation, route}) {
@@ -14,12 +15,24 @@ export default function ScreenB ({navigation, route}) {
    const onBack = ()=>{
     navigation.navigate("Screen_A");
    }
+
+   const [colorValue , setColorValue] = useState('not set')
+
+   const apiCall = async()=>{
+    let response = await axios.get("https://reqres.in/api/products/3");
+    console.log(response);
+    setColorValue(response.data.data.color);
+   }
+
   return (
    <View style={styles.body}>
     <Text style={styles.text}>Screen B</Text>
     <Text style={styles.text}>Welcome  {myName}</Text>
     <Text style={styles.text}>In screen A Button was clicked {clicked} times</Text>
     <Button title='GO to Screen A' onPress={onBack}></Button>
+    <Text style={styles.text}>Calling API</Text>
+    <Button title='Call API' onPress={apiCall}></Button>
+    <Text style={styles.text}>API called and value is {colorValue}</Text>
    </View>
   );
 };
